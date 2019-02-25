60945711dd1785b8f6064608e47e5bf83f9c5983
rumeysa.bukecik <rumeysa.bukecik@404-Y04>
2019-02-25 14:17:42 +0300

    R_SinemaProjesi

---
+++ ProjeFilm/ProjeFilm/Entity/Oyuncu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFilm
+{
+    public class Oyuncu
+    {
+        public int OyuncuID { get; set; }
+        public string OyuncuAdSoyad { get; set; }
+        public string OyuncuDogumTarihi { get; set; }
+
+        public ICollection<Film> OyuncuFilmleri { get; set; }
+    }
+}
---
+++ ProjeFilm/ProjeFilm/Entity/Yonetmen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeFilm
+{
+    public class Yonetmen
+    {
+        public int YonetmenID { get; set; }
+        public string YonetmenAdSoyad { get; set; }
+
+        public ICollection<Film> YonetmenFilmleri { get; set; }
+    }
+}
